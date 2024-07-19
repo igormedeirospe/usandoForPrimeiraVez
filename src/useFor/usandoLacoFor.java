9fd4fac746569b09991a7068824b42368266236b
Igor <igormedeiros407@gmail.com>
2024-07-19 19:08:38 -0300

    commit

--- src/useFor/usandoLacoFor.java
+++ src/useFor/usandoLacoFor.java
@@ -1,2 +1,16 @@
-package useFor;public class usandoLacoFor {
+package useFor;
+
+public class usandoLacoFor {
+    public static void main(String[] args) {
+
+        //O laço "for" em Java é uma estrutura de repetição utilizada para executar um bloco de código um número específico de vezes, geralmente
+        // conhecida pelo número de iterações, que pode ser determinada pelo tamanho de um array ou por um contador definido.
+
+        int numero;
+        numero = 5;
+        for (numero = numero; numero <= 11; numero++){
+                System.out.println(numero);
+        }
+
+    }
 }
